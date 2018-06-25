ea6a084cd948f259a1918907ade1ea70d5c4efea
Allegra Anka <allegra.anka@gmail.com>
2018-06-25 12:00:45 -0400

    completed reverseArrayInPlace function

--- scripts.js
+++ scripts.js
@@ -1,51 +1,23 @@
-// write a range function that takes two arguments: start, and end, and returns an array of all the numbers from start
-// up to and including end
 
-function range(start, end) {
-  var arr = [];
-  if (start > end) {
-    for (i = end; i <= start; i++) {
-      arr.unshift(i);
-    }
+// now, reverse an array in place.
+
+array = ["Hey", "what's", "up", "y'all", "?"];
+arrayTwo = [1, 2, 3, 4, 5];
+arrayThree = ["?", "out", "hang", "Wanna"];
+
+function reverseInPlace(arr) {
+  var length = arr.length,
+      middle = arr.length / 2,
+      placeholder = null;
+
+  for (i = 0; i < middle; i += 1) {
+    placeholder = arr[i];
+    arr[i] = arr[length - 1 - i];
+    arr[length - 1 - i] = placeholder;
   }
-  for (i = start; i <= end; i++) {
-    arr.push(i);
-  }
-  return arr;
+  return arr.join(' ');
 }
 
-console.log(range(2, 12));
-console.log(range(12, 2));
-console.log(range(50, 100));
-
-// now, write a sum function that takes an array of numbers and returns the sum of those numbers
-
-function sum(array) {
-  let total = array.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
-  return total;
-}
-
-var array1 = [1, 2, 3, 4, 5];
-
-document.write(sum(array1));
-
-console.log(sum(range(1, 10)));
-
-// write a function, reverseArray, that takes an array as an argument and returns a NEW array that has the same elements
-// in inverse order
-
-function reverseArray(arr) {
-  let newArr = [];
-  while ( (i = arr.shift()) !== undefined) {
-    console.log(i);
-    newArr.unshift(i);
-  }
-  return newArr;
-}
-
-var array2 = ["Hello", "Cruel", "World", "!"];
-
-console.log(reverseArray(array2));
+document.write(reverseInPlace(array));
+document.write(reverseInPlace(arrayTwo));
+document.write(reverseInPlace(arrayThree));
